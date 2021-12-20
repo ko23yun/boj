9f108d0d828cd8fe9ae586e2fa40783aa630a1b6
ko23yun <ko23yun@naver.com>
2021-12-20 21:30:17 +0900

    단계별 풀이 1부터 하나씩 풀고 있음

--- boj.js
+++ boj.js
@@ -1,19 +1,7 @@
-// add, commit, push 해주기!!!
-// dev/stdin
+// add, commit, push 해주기!!! /dev/stdin
 let fs = require("fs");
-let input = fs.readFileSync("./solve.txt").toString().trim().split("\n");
+let input = fs.readFileSync("./solve.txt").toString().trim();
 
-let T = Number(input.shift());
-let arr = input
-  .shift()
-  .split(" ")
-  .map((v) => +v);
+let N = Number(input);
 
-// 배열에 들어있는 값들이 가능한 모든 경우의 수를 구하고(720가지)
-// 각 값들의 차의 절댓값의 최대합을 구한다라...
-let sum = 1;
-for (let i = 1; i <= T; i++) {
-  sum = sum * i;
-}
-
-console.log(sum);
+console.log(N);
